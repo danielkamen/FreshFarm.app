16a10e668107d1c3e810c7ce496bb944d1d94822
dizonkat <katdizon@gmail.com>
2023-02-11 02:32:44 -0500

    homepage

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import Navigation from "../components/Navigation";
+import About from "../components/About";
+import Callout from "../components/Callout";
+
+export default function HomePage() {
+  return (
+      <div>
+        <Navigation/>
+        <About/>
+        <Callout/>
+      </div>
+  );
+}
