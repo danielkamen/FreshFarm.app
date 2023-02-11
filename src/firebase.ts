b8fb3bb10bf0015c7cb09bd5444381323c3e0291
Daniel Kamen <dsk3110@yahoo.com>
2023-02-11 02:07:02 -0500

    fixed firebase link

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBMuJn_o-0si2VLM3ZOPStRiU5TcbQJGV0",
+  authDomain: "fresh-farm-app.firebaseapp.com",
+  projectId: "fresh-farm-app",
+  storageBucket: "fresh-farm-app.appspot.com",
+  messagingSenderId: "343657294972",
+  appId: "1:343657294972:web:a2961b026e157f9c22edf9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
