13c48991e73317a529e370165625f4e2bf7e75f7
Allen Lin <allenlinsh@gmail.com>
2023-02-11 15:06:52 -0500

    Add tailwind colors and reformat code

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -1,15 +1,15 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase";
-import { collection, addDoc } from "firebase/firestore"; 
+import { collection, addDoc } from "firebase/firestore";
 import { db } from "../firebase";
-import { PRODUCTPAGE } from '../constants/routes'
+import { PRODUCTPAGE } from "../constants/routes";
 import { useForm } from "react-hook-form";
 
 export default function Example() {
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
   const [isBuyer, setIsBuyer] = useState<boolean>(true);
   return (
     <>
@@ -30,41 +30,49 @@ export default function Example() {
                 src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
                 alt="Your Company"
               />
-              <h2 className="mt-6 text-3xl font-bold tracking-tight text-gray-900">Sign up today!</h2>
+              <h2 className="mt-6 text-3xl font-bold tracking-tight text-gray-900">
+                Sign up today!
+              </h2>
             </div>
 
             <div className="mt-8">
               <div className="mt-6">
-                <form id='signup-form' className="space-y-6"
-                  onSubmit={async(event) => {
+                <form
+                  id="signup-form"
+                  className="space-y-6"
+                  onSubmit={async (event) => {
                     event.preventDefault();
                     try {
-                      await createUserWithEmailAndPassword(auth, email, password)
+                      await createUserWithEmailAndPassword(
+                        auth,
+                        email,
+                        password
+                      );
                       if (isBuyer) {
-                        await addDoc(collection(db, 'buyers'), {
-                            email_address: email,
-                            created_at: new Date(),
-                            updated_at: new Date(),
-                            cart: [],
-                            wishlist: [],
-                          });
+                        await addDoc(collection(db, "buyers"), {
+                          email_address: email,
+                          created_at: new Date(),
+                          updated_at: new Date(),
+                          cart: [],
+                          wishlist: [],
+                        });
                       } else {
-                        await addDoc(collection(db, 'sellers'), {
-                            email_address: email,
-                            created_at: new Date(),
-                            updated_at: new Date(),
-                          });
+                        await addDoc(collection(db, "sellers"), {
+                          email_address: email,
+                          created_at: new Date(),
+                          updated_at: new Date(),
+                        });
                       }
                     } catch (e: any) {
-                    if (e.message === 'USER_NOT_FOUND') {
-
-                    }
+                      if (e.message === "USER_NOT_FOUND") {
+                      }
                       console.error(e);
                     }
-                  }}
-                >
+                  }}>
                   <div>
-                    <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                    <label
+                      htmlFor="email"
+                      className="block text-sm font-medium text-gray-700">
                       Email address
                     </label>
                     <div className="mt-1">
@@ -82,7 +90,9 @@ export default function Example() {
                   </div>
 
                   <div className="space-y-1">
-                    <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+                    <label
+                      htmlFor="password"
+                      className="block text-sm font-medium text-gray-700">
                       Password
                     </label>
                     <div className="mt-1">
@@ -107,7 +117,9 @@ export default function Example() {
                         type="checkbox"
                         className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
                       />
-                      <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
+                      <label
+                        htmlFor="remember-me"
+                        className="ml-2 block text-sm text-gray-900">
                         Remember me
                       </label>
                     </div>
@@ -116,10 +128,12 @@ export default function Example() {
                   <div>
                     <button
                       type="submit"
-                      form='signup-form'
-                      className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                      >
-                      <Link to={PRODUCTPAGE} className="text-indigo-600 hover:text-indigo-500"/>
+                      form="signup-form"
+                      className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                      <Link
+                        to={PRODUCTPAGE}
+                        className="text-indigo-600 hover:text-indigo-500"
+                      />
                       Sign up
                     </button>
                   </div>
@@ -137,5 +151,5 @@ export default function Example() {
         </div>
       </div>
     </>
-  )
+  );
 }
