6de975dec7dbc305b396cbfb8811bdae426add28
Daniel Kamen <dsk3110@yahoo.com>
2023-02-11 03:36:44 -0500

    updated routes for  homepage, login, signup, etc

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
 import HomePage from "./pages/HomePage";
+import SignUpPage from './components/SignUpFarmer'
+import SearchBar from './components/Navigation'
+import ProductPage from './pages/ProductPage'
+import VerifyFarmer from './pages/VerifyFarmer'
+
+import {HOME, LOGIN, SIGNUP, SEARCH, PRODUCTPAGE, VERIFYFARMER} from './constants/routes'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom'
+
+
+
 
 function App() {
   return (
-        <HomePage/>
+    <Router>
+      <div className="App">
+        <>
+          <Routes>
+            <Route path={HOME} element={<HomePage/>}/>
+            <Route path={LOGIN} element={<></>} />
+            <Route path={SIGNUP} element={<SearchBar/>} />
+            <Route path={SEARCH} element={<SignUpPage />} />
+            <Route path={PRODUCTPAGE} element={<ProductPage />} />
+            <Route path={VERIFYFARMER} element={<VerifyFarmer />} />
+        </Routes>
+        </>
+      </div>
+        
+    </Router>
+      
   );
 }
 
---
+++ src/constants/routes.ts
@@ -0,0 +1,6 @@
+export const HOME = '/'
+export const LOGIN = '/login'
+export const SIGNUP = '/signup'
+export const SEARCH = '/search'
+export const PRODUCTPAGE = '/productpage'
+export const VERIFYFARMER = '/verifyfarmer'
