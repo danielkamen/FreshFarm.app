ce18f4ea24def227da48d949c32fef38a971f35e
dizonkat <katdizon@gmail.com>
2023-02-11 17:53:46 -0500

    product page looking good

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -45,61 +45,55 @@ const products = [
   },
   {
     id: 1,
-    name: "Earthen Bottle",
+    name: "Bananas",
     href: "#",
-    price: "$48",
     imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg",
+      "https://suncitycenterphotos.com/wp-content/uploads/2014/06/Banana-prices-in-Sun-City-Center-Florida1.jpg",
     imageAlt:
       "Tall slender porcelain bottle with natural clay textured body and cork stopper.",
   },
   {
     id: 2,
-    name: "Nomad Tumbler",
+    name: "Garlic",
     href: "#",
-    price: "$35",
     imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg",
+      "https://grocycle.com/wp-content/uploads/2022/07/Garlic-at-a-market-1024x655.jpg",
     imageAlt:
       "Olive drab green insulated bottle with flared screw lid and flat top.",
   },
   {
     id: 3,
-    name: "Focus Paper Refill",
+    name: "Brocollini",
     href: "#",
-    price: "$89",
     imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg",
+      "http://cdn.shopify.com/s/files/1/0206/9470/files/Pick_of_the_Week_-_Broccolini.jpg?v=1502173625",
     imageAlt:
       "Person using a pen to cross a task off a productivity paper card.",
   },
   {
     id: 4,
-    name: "Machined Mechanical Pencil",
+    name: "Sweet Potatoes",
     href: "#",
-    price: "$35",
     imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg",
+      "https://www.agmrc.org/media/cms/swpotatoescomp_B6BE5ECFB4E85.jpg",
     imageAlt:
       "Hand holding black machined steel mechanical pencil with brass tip and top.",
   },
   {
     id: 1,
-    name: "Earthen Bottle",
+    name: "Starfruit",
     href: "#",
-    price: "$48",
     imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg",
+      "https://media.istockphoto.com/id/112860361/photo/photograph-of-multiple-starfruit.jpg?s=612x612&w=0&k=20&c=K7EaHcMsr_1P4tj1rcEO-ZQcW0BYnhqKKyw-nKR5xBE=",
     imageAlt:
       "Tall slender porcelain bottle with natural clay textured body and cork stopper.",
   },
   {
     id: 2,
-    name: "Nomad Tumbler",
+    name: "Dragonfruit",
     href: "#",
-    price: "$35",
     imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg",
+      "https://media-cdn.tripadvisor.com/media/photo-s/06/8e/72/2e/keauhou-farmer-s-market.jpg",
     imageAlt:
       "Olive drab green insulated bottle with flared screw lid and flat top.",
   },
@@ -107,59 +101,17 @@ const products = [
     id: 3,
     name: "Focus Paper Refill",
     href: "#",
-    price: "$89",
     imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg",
+      "https://extension.unh.edu/sites/default/files/styles/max_width_480px/public/migrated_images/blue-1326154_1280.jpg?itok=RA0CMEsN",
     imageAlt:
       "Person using a pen to cross a task off a productivity paper card.",
   },
   {
     id: 4,
-    name: "Machined Mechanical Pencil",
+    name: "Peaches",
     href: "#",
-    price: "$35",
     imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg",
-    imageAlt:
-      "Hand holding black machined steel mechanical pencil with brass tip and top.",
-  },
-  {
-    id: 1,
-    name: "Earthen Bottle",
-    href: "#",
-    price: "$48",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg",
-    imageAlt:
-      "Tall slender porcelain bottle with natural clay textured body and cork stopper.",
-  },
-  {
-    id: 2,
-    name: "Nomad Tumbler",
-    href: "#",
-    price: "$35",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg",
-    imageAlt:
-      "Olive drab green insulated bottle with flared screw lid and flat top.",
-  },
-  {
-    id: 3,
-    name: "Focus Paper Refill",
-    href: "#",
-    price: "$89",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg",
-    imageAlt:
-      "Person using a pen to cross a task off a productivity paper card.",
-  },
-  {
-    id: 4,
-    name: "Machined Mechanical Pencil",
-    href: "#",
-    price: "$35",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg",
+      "https://img.freepik.com/premium-photo/fresh-yellow-peaches-cardboard-crates-farmers-market_707345-24.jpg?w=2000",
     imageAlt:
       "Hand holding black machined steel mechanical pencil with brass tip and top.",
   },
