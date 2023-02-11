2ea338d36df08d2e7f7151d4cefc70e373d9165c
dizonkat <katdizon@gmail.com>
2023-02-11 18:20:20 -0500

    Changes by julia

--- src/components/Callout.tsx
+++ src/components/Callout.tsx
@@ -48,10 +48,10 @@ export default function Callout() {
     <div className="bg-gray-100">
       <div className="mx-auto max-w-6xl px-4 sm:px-6 lg:px-4">
         <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
-          <div className="mt-6 space-y-12 lg:grid lg:grid-cols-2 lg:gap-x-6 lg:space-y-0">
+          <div className="mt-6 space-y-10 lg:grid lg:grid-cols-2 lg:gap-x-20 lg:space-y-0">
             {callouts.map((callout) => (
               <div key={callout.name} className="group relative">
-                <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1">
+                <div className="relative h-90 w-full overflow-hidden rounded-lg bg-white group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1">
                   <img
                     src={callout.imageSrc}
                     alt={callout.imageAlt}
