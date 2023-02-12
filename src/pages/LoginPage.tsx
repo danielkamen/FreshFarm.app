8a084c9e91fbcd3f042ad7dbe454bc5066816447
Kelly <>
2023-02-11 23:47:37 -0500

    reset password

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,17 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, Fragment, useRef } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { signInWithEmailAndPassword, sendPasswordResetEmail } from "firebase/auth";
+import { Dialog, Transition } from '@headlessui/react'
 import { auth } from "../firebase";
-import { FORGETPASSWORDPAGE, HOME, SIGNUP } from "../constants/routes";
+import { HOME, SIGNUP } from "../constants/routes";
 import { useUserContext } from "../contexts/useUserContext";
 import logo from "./farmfreshnavlogo.gif";
 
 export default function Example() {
+  let navigate = useNavigate();
+  const { setUser } = useUserContext();
+
   const [error, setError] = useState({ email: "", password: "" });
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
-  let navigate = useNavigate();
-  const { setUser } = useUserContext();
+  const [forgotPasswordIsOpen, setForgotPasswordIsOpen] = useState<boolean>(false);
+  const [resetPasswordEmail,setResetPasswordEmail] = useState<string>('');
+  const cancelButtonRef = useRef(null)
 
   return (
     <>
@@ -135,11 +140,17 @@ export default function Example() {
                     </div>
 
                     <div className="text-sm">
-                      <Link
-                        to={FORGETPASSWORDPAGE}
-                        className="font-medium text-secondary hover:text-secondary-accent">
+                      <button
+                        type="button"
+                        className="font-medium text-secondary hover:text-secondary-accent"
+                        onClick={(e) => {
+                          e.preventDefault();
+                          setForgotPasswordIsOpen(!forgotPasswordIsOpen);
+                        }}
+                        onKeyDown={(e) => { e.key === 'Enter' && e.preventDefault(); }}
+                        >
                         Forgot your password?
-                      </Link>
+                      </button>
                     </div>
                   </div>
 
@@ -164,6 +175,86 @@ export default function Example() {
           />
         </div>
       </div>
+      {<Transition.Root show={forgotPasswordIsOpen} as={Fragment}>
+      <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={setForgotPasswordIsOpen}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 z-10 overflow-y-auto">
+          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                  <div className="sm:flex sm:items-start">
+                    <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                      <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900">
+                        Reset Password
+                      </Dialog.Title>
+                      <div className="mt-2">
+                        <p className="text-sm text-gray-500 mb-2">
+                          An email will be sent to the given address to reset your password.
+                        </p>
+                        <input
+                          id="email-login"
+                          name="email"
+                          autoComplete="email"
+                          placeholder="Email address"
+                          required
+                          className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-secondary focus:outline-none focus:ring-secondary sm:text-sm"
+                          value={resetPasswordEmail}
+                          onChange={(event) => setResetPasswordEmail(event.target.value)}
+                      />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                  <button
+                    type="button"
+                    className="inline-flex w-full justify-center rounded-md border border-transparent bg-secondary px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-secondary-accent focus:outline-none focus:ring-2 focus:ring-secondary focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
+                    onClick={async () => {
+                      try {
+                        await sendPasswordResetEmail(auth, resetPasswordEmail);
+                        setResetPasswordEmail('');
+                      } catch (e) {
+                        console.error(e);
+                      }
+                    }}
+                  >
+                    Reset password
+                  </button>
+                  <button
+                    type="button"
+                    className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                    onClick={() => setForgotPasswordIsOpen(false)}
+                    ref={cancelButtonRef}
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>}
     </>
   );
 }
