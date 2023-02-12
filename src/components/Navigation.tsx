0e89945d492f809fcc609122854354ca848cecbb
Kelly <>
2023-02-11 19:34:46 -0500

    set up profile page, add sign out

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,7 +1,7 @@
 import { Fragment, useEffect, useState } from "react";
-import logo from "./farmfreshnavlogo.gif";
-import { Popover, Transition } from "@headlessui/react";
 import { Link } from "react-router-dom";
+import { signOut } from "@firebase/auth";
+import { Menu, Popover, Transition } from "@headlessui/react";
 import {
   Bars3Icon,
   BookmarkSquareIcon,
@@ -29,8 +29,15 @@ import {
   CARTPAGE,
   HOME,
   MEETFARMERSPAGE,
+  PROFILE
 } from "../constants/routes";
+import { auth } from "../firebase";
 import { useUserContext } from "../contexts/useUserContext";
+import logo from "./farmfreshnavlogo.gif";
+
+function classNames(...classes: any[]) {
+  return classes.filter(Boolean).join(' ')
+}
 
 const features = [
   {
@@ -55,7 +62,7 @@ const features = [
 
 export default function Navigation() {
   const { user } = useUserContext();
-
+  console.log(user)
   return (
     <Popover className="sticky top-0 bg-white z-10">
       <div
@@ -195,10 +202,10 @@ export default function Navigation() {
                   </Link>
                 </>
               ) : (
-                <div className="ml-4 flow-root lg:ml-6">
+                <div className="ml-4 lg:ml-6 flex flex-row items-center">
                   <Link
                     to={CARTPAGE}
-                    className="group -m-2 flex items-center p-2">
+                    className="group flex items-center p-2">
                     <ShoppingBagIcon
                       className="h-6 w-6 flex-shrink-0 text-primary group-hover:text-primary-accent"
                       aria-hidden="true"
@@ -208,6 +215,73 @@ export default function Navigation() {
                     </span>
                     <span className="sr-only">items in cart, view bag</span>
                   </Link>
+                  <div className="-my-2 -mr-2">
+                    <Menu as="div" className="relative inline-block text-left">
+                      <div>
+                        <Menu.Button className="items-center inline-flex w-full justify-center rounded-md border border-gray-300 bg-primary px-4 py-2 text-sm font-medium text-white shadow-sm hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100">
+                          Options
+                          <ChevronDownIcon className="-mr-1 ml-2 h-6 w-6" aria-hidden="true" />
+                        </Menu.Button>
+                      </div>
+
+                      <Transition
+                        as={Fragment}
+                        enter="transition ease-out duration-100"
+                        enterFrom="transform opacity-0 scale-95"
+                        enterTo="transform opacity-100 scale-100"
+                        leave="transition ease-in duration-75"
+                        leaveFrom="transform opacity-100 scale-100"
+                        leaveTo="transform opacity-0 scale-95"
+                      >
+                        <Menu.Items className="absolute right-0 z-50 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                          <div className="py-1">
+                          <Menu.Item>
+                              {({ active }) => (
+                                <a
+                                  href="#"
+                                  className={classNames(
+                                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                    'block px-4 py-2 text-sm'
+                                  )}
+                                >
+                                  Signed in as <strong>{user.email}</strong>
+                                </a>
+                              )}
+                            </Menu.Item>
+                            <Menu.Item>
+                              {({ active }) => (
+                                <a
+                                  href={PROFILE}
+                                  className={classNames(
+                                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                    'block px-4 py-2 text-sm'
+                                  )}
+                                >
+                                  <Link to={PROFILE} />
+                                  Account settings
+                                </a>
+                              )}
+                            </Menu.Item>
+                            <form onSubmit={() => signOut(auth)}>
+                              <Menu.Item>
+                                {({ active }) => (
+                                  <button
+                                    type="submit"
+                                    className={classNames(
+                                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                      'block w-full px-4 py-2 text-left text-sm'
+                                    )}
+                                  >
+                                    Sign out
+                                  </button>
+                                )}
+                              </Menu.Item>
+                            </form>
+                          </div>
+                        </Menu.Items>
+                      </Transition>
+                    </Menu>
+                  </div>
                 </div>
               )}
             </div>
