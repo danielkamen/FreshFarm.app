6c0f7182cf29444d1cc111be9388272ed9f250a2
dizonkat <katdizon@gmail.com>
2023-02-11 22:16:21 -0500

    login page GOOD

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, Fragment, useRef, } from "react";
+import { Dialog, Transition } from '@headlessui/react'
+import { ExclamationTriangleIcon } from '@heroicons/react/24/outline'
 import { useNavigate } from "react-router-dom";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase";
-import { HOME, PRODUCTPAGE } from "../constants/routes";
+import { HOME } from "../constants/routes";
 import { useUserContext } from "../contexts/useUserContext";
 import logo from "./farmfreshnavlogo.gif";
 
@@ -10,12 +12,17 @@ export default function Example() {
   const [error, setError] = useState({ email: "", password: "" });
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [resetPswdEmail, setResetPswdEmail] = useState<string>('');
+
   let navigate = useNavigate();
   const { setUser } = useUserContext();
 
+  const cancelButtonRef = useRef(null)
+
   return (
-    <>
-      {/*
+      <>
+        {/*
         This example requires updating your template:
 
         ```
@@ -23,157 +30,235 @@ export default function Example() {
         <body class="h-full">
         ```
       */}
-      <div className="bflex min-h-full">
-        <div className="flex flex-1 flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
-          <div className="mx-auto w-full max-w-sm lg:w-96">
-            <div>
-              <img
-                className="h-12 w-auto"
-                src={logo}
-                alt="Your Company"
-              />
-              <h2 className="mt-6 text-3xl font-bold tracking-tight text-gray-900">
-                Sign in to your account
-              </h2>
-              <p className="mt-2 text-sm text-gray-600">
-                Or{" "}
-                <a
-                  href="#"
-                  className="font-medium text-indigo-600 hover:text-indigo-500">
-                  start your 14-day free trial
-                </a>
-              </p>
+        {isOpen ?  <Transition.Root show={isOpen} as={Fragment}>
+          <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={setIsOpen}>
+            <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+            >
+              <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+            </Transition.Child>
+
+            <div className="fixed inset-0 z-10 overflow-y-auto">
+              <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                <Transition.Child
+                    as={Fragment}
+                    enter="ease-out duration-300"
+                    enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                    enterTo="opacity-100 translate-y-0 sm:scale-100"
+                    leave="ease-in duration-200"
+                    leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                    leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                >
+                  <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                    <div className="ml-5 mt-5 md:col-span-1">
+                      <h3 className="text-lg font-medium leading-6 text-gray-900">Enter email to reset your password.</h3>
+                      <p className="mt-1 text-sm text-gray-500">
+                        An email will be sent with a code to reset your password.
+                      </p>
+                    </div>
+                    <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                      <input
+                          id="email-reset"
+                          name="email-reset"
+                          autoComplete="email"
+                          required
+                          className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                          value={resetPswdEmail}
+                          onChange={(event) => setResetPswdEmail(event.target.value)}
+                      />
+                    </div>
+                    <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                      <button
+                          type="button"
+                          className="inline-flex w-full justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
+                          onClick={async () => {
+                            try {
+                              await sendPasswordResetEmail(auth, resetPswdEmail);
+                              setResetPswdEmail('');
+                              setIsOpen(false);
+                            } catch (e) {
+                              console.error(e);
+                            }
+                          }}
+                      >
+                        Reset password
+                      </button>
+                      <button
+                          type="button"
+                          className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                          onClick={() => setIsOpen(false)}
+                          ref={cancelButtonRef}
+                      >
+                        Cancel
+                      </button>
+                    </div>
+                  </Dialog.Panel>
+                </Transition.Child>
+              </div>
             </div>
-            <div className="mt-8">
-              <div className="mt-6">
-                <form
-                  id="login-form"
-                  className="space-y-6"
-                  onSubmit={async (event) => {
-                    event.preventDefault();
-                    try {
-                      const authUser = await signInWithEmailAndPassword(
-                        auth,
-                        email,
-                        password
-                      );
-                      setUser(authUser.user);
-                      navigate(HOME);
-                    } catch (e: any) {
-                      const err = (e as Error).message;
-                      console.log(err);
-                      if (
-                        err ===
-                        "Firebase: Access to this account has been temporarily disabled due to many failed login attempts. You can immediately restore it by resetting your password or you can try again later. (auth/too-many-requests)."
-                      ) {
-                        setError({
-                          email: "",
-                          password:
-                            "Too many login attempts. Please try again later.",
-                        });
-                      } else {
-                        setError({
-                          email: "",
-                          password: "Email or password is incorrect.",
-                        });
-                      }
-                      console.error(e);
-                    }
-                  }}>
-                  <div>
-                    <label
-                      htmlFor="email"
-                      className="block text-sm font-medium text-gray-700">
-                      Email address
-                    </label>
-                    <div className="mt-1">
-                      <input
-                        id="email-login"
-                        name="email"
-                        autoComplete="email"
-                        required
-                        className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                        value={email}
-                        onChange={(event) => setEmail(event.target.value)}
-                      />
-                      {error.email !== "" && (
-                        <div className="mb-3 text-normal text-red-500 ">
-                          {error.email}
-                        </div>
-                      )}
-                    </div>
-                  </div>
-
-                  <div className="space-y-1">
-                    <label
-                      htmlFor="password"
-                      className="block text-sm font-medium text-gray-700">
-                      Password
-                    </label>
-                    <div className="mt-1">
-                      <input
-                        id="password"
-                        name="password"
-                        type="password"
-                        autoComplete="current-password"
-                        required
-                        className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                        value={password}
-                        onChange={(event) => setPassword(event.target.value)}
-                      />
-                      {error.password !== "" && (
-                        <div className="mb-3 text-normal text-red-500 ">
-                          {error.password}
-                        </div>
-                      )}
-                    </div>
-                  </div>
-
-                  <div className="flex items-center justify-between">
-                    <div className="flex items-center">
-                      <input
-                        id="remember-me"
-                        name="remember-me"
-                        type="checkbox"
-                        className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                      />
+          </Dialog>
+        </Transition.Root>: <></>}
+        <div className="flex bg-[#F8F9F7] min-h-full">
+          <div className="flex flex-1 flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+            <div className="mx-auto w-full max-w-sm lg:w-96">
+              <div>
+                <img
+                    className="h-12 w-auto"
+                    src={logo}
+                    alt="Your Company"
+                />
+                <h2 className="mt-6 text-3xl font-bold tracking-tight text-gray-900">
+                  Sign in to your account
+                </h2>
+                <p className="mt-2 text-sm text-gray-600">
+                  Or{" "}
+                  <a
+                      href="#"
+                      className="font-medium text-indigo-600 hover:text-indigo-500">
+                    start your 14-day free trial
+                  </a>
+                </p>
+              </div>
+              <div className="mt-8">
+                <div className="mt-6">
+                  <form
+                      id="login-form"
+                      className="space-y-6"
+                      onSubmit={async (event) => {
+                        event.preventDefault();
+                        try {
+                          const authUser = await signInWithEmailAndPassword(
+                              auth,
+                              email,
+                              password
+                          );
+                          setUser(authUser.user);
+                          navigate(HOME);
+                        } catch (e: any) {
+                          const err = (e as Error).message;
+                          console.log(err);
+                          if (
+                              err ===
+                              "Firebase: Access to this account has been temporarily disabled due to many failed login attempts. You can immediately restore it by resetting your password or you can try again later. (auth/too-many-requests)."
+                          ) {
+                            setError({
+                              email: "",
+                              password:
+                                  "Too many login attempts. Please try again later.",
+                            });
+                          } else {
+                            setError({
+                              email: "",
+                              password: "Email or password is incorrect.",
+                            });
+                          }
+                          console.error(e);
+                        }
+                      }}>
+                    <div>
                       <label
-                        htmlFor="remember-me"
-                        className="ml-2 block text-sm text-gray-900">
-                        Remember me
+                          htmlFor="email"
+                          className="block text-sm font-medium text-gray-700">
+                        Email address
                       </label>
+                      <div className="mt-1">
+                        <input
+                            id="email-login"
+                            name="email"
+                            autoComplete="email"
+                            required
+                            className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                            value={email}
+                            onChange={(event) => setEmail(event.target.value)}
+                        />
+                        {error.email !== "" && (
+                            <div className="mb-3 text-normal text-red-500 ">
+                              {error.email}
+                            </div>
+                        )}
+                      </div>
                     </div>
 
-                    <div className="text-sm">
-                      <a
-                        href="#"
-                        className="font-medium text-indigo-600 hover:text-indigo-500">
-                        Forgot your password?
-                      </a>
+                    <div className="space-y-1">
+                      <label
+                          htmlFor="password"
+                          className="block text-sm font-medium text-gray-700">
+                        Password
+                      </label>
+                      <div className="mt-1">
+                        <input
+                            id="password"
+                            name="password"
+                            type="password"
+                            autoComplete="current-password"
+                            required
+                            className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                            value={password}
+                            onChange={(event) => setPassword(event.target.value)}
+                        />
+                        {error.password !== "" && (
+                            <div className="mb-3 text-normal text-red-500 ">
+                              {error.password}
+                            </div>
+                        )}
+                      </div>
                     </div>
-                  </div>
 
-                  <div>
-                    <button
-                      type="submit"
-                      form="login-form"
-                      className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                      Sign in
-                    </button>
-                  </div>
-                </form>
+                    <div className="flex items-center justify-between">
+                      <div className="flex items-center">
+                        <input
+                            id="remember-me"
+                            name="remember-me"
+                            type="checkbox"
+                            className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                        />
+                        <label
+                            htmlFor="remember-me"
+                            className="ml-2 block text-sm text-gray-900">
+                          Remember me
+                        </label>
+                      </div>
+
+                      <div className="text-sm">
+                        <a
+                            onClick={(event) => {
+                              event.preventDefault();
+                              setIsOpen(!isOpen);
+                              setResetPswdEmail('');
+                            }}
+                            className="font-medium text-indigo-600 hover:text-indigo-500">
+                          Forgot your password?
+                        </a>
+                      </div>
+                    </div>
+
+                    <div>
+                      <button
+                          type="submit"
+                          form="login-form"
+                          className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                        Sign in
+                      </button>
+                    </div>
+                  </form>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="relative hidden w-0 flex-1 lg:block">
-          <img
-            className="absolute inset-0 h-full w-full object-cover"
-            src="https://img.freepik.com/premium-photo/happy-farmers-harvest-rice-together-with-sickles-during-day_8595-18204.jpg?w=1480"
-            alt=""
-          />
-        </div>
-      </div>
-    </>
+          <div className="relative hidden w-0 flex-1 lg:block">
+            <img
+                className="absolute inset-0 h-full w-full object-cover"
+                src="https://img.freepik.com/premium-photo/happy-farmers-harvest-rice-together-with-sickles-during-day_8595-18204.jpg?w=1480"
+                alt=""
+            />
+          </div>
+        </div>https://code-with-me.global.jetbrains.com/CpuJvbu0e9bLUz9MpLJBVQ#p=IU&fp=5E853004BC6179DA60E4CEF02922FFB875CB35520EE895BFB65A91E1F2D924CB
+      </>
   );
 }
